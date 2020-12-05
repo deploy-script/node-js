efd93926781f341deb47a202431951482f591a2d
Lawrence Cherone <lawrence@cherone.co.uk>
2020-12-05 02:06:29 +0000

    .

--- script.sh
+++ script.sh
@@ -54,6 +54,7 @@ setup_environment() {
 ##
 install_nvm() {
     wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+    
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" 
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
     
