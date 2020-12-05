097d0e2085c778ff53ce03eb4b271e30f6e49224
Lawrence Cherone <lawrence@cherone.co.uk>
2020-12-05 04:10:45 +0000

    .

--- script.sh
+++ script.sh
@@ -56,8 +56,8 @@ install_nvm() {
     wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
     
     export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
     
     nvm install $NODE_VERSION
     
